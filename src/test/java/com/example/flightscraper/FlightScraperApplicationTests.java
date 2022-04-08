dab9a40c3b6abdc20a60e85b519d5d2ff7e1d39a
Olivier COUJANDASSAMY <olivier.coujan@gmail.com>
2022-04-08 11:15:08 +0200

    Init project

---
+++ src/test/java/com/example/flightscraper/FlightScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flightscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
